f4a043c44fc1fb88b12669c1eaea49fb372a1ee3
Dylan Vu <dylanvu9@gmail.com>
2024-05-26 05:22:16 -0700

    able to view public enemy

--- frontend/sexism sniper/src/App.tsx
+++ frontend/sexism sniper/src/App.tsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+// import viteLogo from '/vite.svg'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // obtain the worst offender from the database, this is their picture URL
+  const [worstOffender, setWorstOffender] = useState("");
+
+  // make api call to get worst offender
+  useEffect(() => {
+    fetch("http://localhost:3000/topScore")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setWorstOffender(data.url);
+      });
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Sexism Sniper</h1>
+
+      <h2>Public Enemy #1</h2>
+
+      {/* access database to find person with most red flags, place person's face here */}
+      <img
+        src={worstOffender.length > 0 ? worstOffender : reactLogo}
+        alt="Wanted Dead or Alive"
+      />
+
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        {/* calculate bounty based on number of red flags */}
+        <h3>Bounty: $100</h3>
+        {/* link to casey's venmo */}
+        <button>Pay Your Bounty</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
+{
+  /* <button onClick={() => setCount((count) => count + 1)}> count is {count}</button> */
+}
